7dd762850a26c850c6a0e19415e2f1a4f9abf62e
Hena <henadii.shevchyk@velocloud.net>
2018-12-07 12:00:00 +0200

    implement singleton pattern using decorator as function

--- singelton_using_decorator.py
+++ singelton_using_decorator.py
@@ -1,4 +1,5 @@
 # Solution how to implement Singleton pattern using decorator.
+# Case 1. Using class as decorator
 
 class dec(object):
     obj_instance = None
@@ -21,3 +22,27 @@ a1 = A()
 a2 = A()
 
 assert a1 == a2
+
+# Case 2. Using function as decorator
+
+_instance = None
+
+
+def dec1(func):
+    def wrapper(*args, **kwargs):
+        global _instance
+        if not _instance:
+            _instance = func()
+        return _instance
+    return wrapper
+
+
+@dec1
+class B(object):
+    pass
+
+
+b1 = B()
+b2 = B()
+
+assert b1 == b2
