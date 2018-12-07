1a52c8321b92abb2646a1ec454e8aab0e34fea4c
Hena <henadii.shevchyk@velocloud.net>
2018-12-07 13:06:24 +0200

    add ability to pass arguments

--- singelton_using_decorator.py
+++ singelton_using_decorator.py
@@ -1,6 +1,7 @@
 # Solution how to implement Singleton pattern using decorator.
 # Case 1. Using class as decorator
 
+
 class dec(object):
     obj_instance = None
 
@@ -8,8 +9,8 @@ class dec(object):
         self.func = func
 
     def __call__(self, *args, **kwargs):
-        if not self.obj_instance:
-            self.obj_instance = self.func()
+        if self.obj_instance is None:
+            self.obj_instance = self.func(*args, **kwargs)
         return self.obj_instance
 
 
@@ -32,7 +33,7 @@ def dec1(func):
     def wrapper(*args, **kwargs):
         global _instance
         if not _instance:
-            _instance = func()
+            _instance = func(*args, **kwargs)
         return _instance
     return wrapper
 
